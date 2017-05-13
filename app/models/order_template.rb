349b77f5613da77119589006f55996c2f5e56686
Aram Zadikian <brancusi@gmail.com>
2017-05-12 21:48:14 -0700

    wip

--- app/models/order_template.rb
+++ app/models/order_template.rb
@@ -2,6 +2,6 @@ class OrderTemplate < ActiveRecord::Base
 
   belongs_to :location
 
-  has_many :order_template_items
-  has_many :order_template_days
+  has_many :order_template_items, :dependent => :destroy, autosave: true
+  has_many :order_template_days, :dependent => :destroy, autosave: true
 end
